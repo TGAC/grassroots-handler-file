401203c1fc32479a508f3615a154e0f85cfe0b1a
Simon Tyrrell <tyrrell.simon@gmail.com>
2018-10-07 18:28:03 +0100

    eclipse cdt 2018-19 decided to refactor all of the include declarations adding relative paths, all without any notifications whatsoever. Whoever decided this was a good idea, er, it was not

--- src/file_handler.c
+++ src/file_handler.c
@@ -13,9 +13,10 @@
 ** See the License for the specific language governing permissions and
 ** limitations under the License.
 */
+#include "file_handler.h"
+
 #include <string.h>
 
-#include "file_handler.h"
 #include "memory_allocations.h"
 #include "data_resource.h"
 
